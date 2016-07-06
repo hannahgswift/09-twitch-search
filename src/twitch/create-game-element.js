5d784dc041b2a3b3dbb3b6150b366567fea02d33
Hannah Swift <h.graceswift@gmail.com>
2016-07-06 16:23:09 -0500

    Left side styling

--- src/twitch/create-game-element.js
+++ src/twitch/create-game-element.js
@@ -31,11 +31,11 @@ export default function createGameElement(game) {
   container.classList.add('game-item');
   container.innerHTML = `
     <div class="game-item">
-      <div>
+      <div class="pic-container">
         ${pic}
       </div>
         <h3 class="game-item__name">${name}</h3>
-        <h3 class="game-item__popularity">${popularity}</h3>
+        <h5 class="game-item__popularity">${popularity}</h5>
     </div>
   `;
 
